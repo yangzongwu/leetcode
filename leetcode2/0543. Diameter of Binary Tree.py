0e8301a5f9b54b57d89ff954813ff541cf3280f9
zongwu <yangzong5@gmail.com>
2019-01-26 13:25:37 -0800

    Update 0543. Diameter of Binary Tree.py

--- leetcode2/0543. Diameter of Binary Tree.py
+++ leetcode2/0543. Diameter of Binary Tree.py
@@ -45,3 +45,34 @@ class Solution(object):
         else:
             root.val=1+max(self.remarktree(root.left).val,+self.remarktree(root.right).val)
         return root
+
+ ################################################################################
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        
+        self.h=0
+        
+        def calheight(root):
+            if not root:
+                return 0
+            left_height=calheight(root.left)
+            right_height=calheight(root.right)
+            self.h=max(self.h,left_height+right_height)
+            return 1+max(left_height,right_height)
+        
+        calheight(root)
+        
+        return self.h
