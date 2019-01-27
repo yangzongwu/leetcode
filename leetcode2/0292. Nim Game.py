5edd0b0b691360f99257f5de71b338568ed5ea16
zongwu <yangzong5@gmail.com>
2019-01-26 23:40:18 -0800

    Create 0292. Nim Game.py

---
+++ leetcode2/0292. Nim Game.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n%4==0:
+            return False
+        return True
