433da80412affbfc91c020e1965085d7d27e6b9d
zongwu <yangzong5@gmail.com>
2019-02-05 12:08:19 -0800

    Create 0075. Sort Colors.py

---
+++ leetcode2/0075. Sort Colors.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sortColors(self, nums: 'List[int]') -> 'None':
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left=0
+        while nums[left]==0:
+            left+=1
+        right=len(nums)-1
+        while nums[right]==2:
+            right-=1
+        cur=left
+        while cur<=right:
+            if nums[cur]==0:
+                nums[left],nums[cur]=nums[cur],nums[left]
+                left+=1
+                cur+=1
+            elif nums[cur]==2:
+                nums[right],nums[cur]=nums[cur],nums[right]
+                right-=1
+            else:
+                cur+=1
