eb1a85d9af21fe9998e0e90a0c1f3e6d4f3674ad
zongwu <yangzong5@gmail.com>
2019-02-08 22:06:30 -0800

    Create 0509. Fibonacci Number.py

---
+++ leetcode2/0509. Fibonacci Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, N: 'int') -> 'int':
+        if N==0:
+            return 0
+        if N==1:
+            return 1
+        dp=[0,1]
+        for i in range(2,N+1):
+            dp.append(dp[-1]+dp[-2])
+        return dp[-1]
