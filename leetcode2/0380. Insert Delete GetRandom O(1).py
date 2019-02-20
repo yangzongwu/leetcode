e716bba1399fd461158724e60fd5a07b74ce39da
zongwu <yangzong5@gmail.com>
2019-02-19 17:19:39 -0800

    Create 0380. Insert Delete GetRandom O(1).py

---
+++ leetcode2/0380. Insert Delete GetRandom O(1).py
@@ -0,0 +1,42 @@
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums=set()
+
+    def insert(self, val: 'int') -> 'bool':
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.nums:
+            return False
+        self.nums.add(val)
+        return True
+        
+        
+
+    def remove(self, val: 'int') -> 'bool':
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val not in self.nums:
+            return False
+        self.nums.remove(val)
+        return True
+        
+
+    def getRandom(self) -> 'int':
+        """
+        Get a random element from the set.
+        """
+        return random.choice(list(self.nums))
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
