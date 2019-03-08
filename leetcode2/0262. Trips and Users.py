1a00d39ce201c82e416672bc1dc4cad8daeb5dc5
zongwu <yangzong5@gmail.com>
2019-03-08 00:50:19 -0800

    Create 0262. Trips and Users.py

---
+++ leetcode2/0262. Trips and Users.py
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+select tb1.Request_at as 'Day', ROUND(sum(case
+                                            when tb1.Status='completed' then 0
+                                            else 1
+                                            end)/count(*),2) as 'Cancellation Rate'
+from Trips as tb1
+inner join Users as tb2
+on tb1.Client_Id=tb2.Users_Id and tb2.Banned='No'
+inner join Users as tb3
+on tb1.Driver_Id =tb3.Users_Id and tb2.Banned='No'
+where tb1.Request_at between '2013-10-01' and '2013-10-03'
+group by tb1.Request_at
+order by Day asc;
+
