ff5c692c18e12793c8b6ab2fef522c6028325d76
zongwu <yangzong5@gmail.com>
2019-03-15 14:06:00 -0700

    Create 0035. Search Insert Position.cs

---
+++ C#/0035. Search Insert Position.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+               right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
