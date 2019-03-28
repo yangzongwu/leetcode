4701d79397669b7e2cc60fef13bcd2a1262b538d
zongwu <yangzong5@gmail.com>
2019-03-28 13:02:53 -0700

    Create 0783. Minimum Distance Between BST Nodes.cs

---
+++ C#/0783. Minimum Distance Between BST Nodes.cs
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int MinDiffInBST(TreeNode root) {
+        Stack<TreeNode> rootStack=new Stack<TreeNode>();
+        TreeNode node=root;
+        int preValue=-1;
+        int rep=int.MaxValue;
+        while(node!=null || rootStack.Count()!=0){
+            while(node!=null){
+                rootStack.Push(node);
+                node=node.left;
+            }
+            if(rootStack.Count()!=0){
+                TreeNode curNode=rootStack.Pop();
+                if(preValue==-1){
+                    preValue=curNode.val;
+                }
+                else{
+                    rep=Math.Min(rep,curNode.val-preValue);
+                    preValue=curNode.val;
+                }
+                node=curNode.right;
+            }
+        }
+        return rep;
+    }
+}
