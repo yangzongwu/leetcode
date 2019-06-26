0cecb4e8228608c9549825de3a541f21a433b773
zongwu <yangzong5@gmail.com>
2019-06-26 12:10:07 -0700

    Update 0022. Generate Parentheses.py

--- 20190519python/0022. Generate Parentheses.py
+++ 20190519python/0022. Generate Parentheses.py
@@ -1,12 +1,12 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        rep=set()
+        rep=[]
         self.get_generateParenthesis(n,0,"",rep)
-        return list(rep)
+        return rep
     
     def get_generateParenthesis(self,n,flag,res,rep):
         if n==0 and flag==0:
-            rep.add(res)
+            rep.append(res)
         elif n==0 and flag>0:
             self.get_generateParenthesis(n,flag-1,res+')',rep)
         elif n>0 and flag==0:
