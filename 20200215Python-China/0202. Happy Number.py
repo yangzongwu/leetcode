1a96a930ae949061459fc4794dafa2d5f9f309b5
zongwu <yangzong5@gmail.com>
2020-02-20 15:14:08 +0800

    Create 0202. Happy Number.py

---
+++ 20200215Python-China/0202. Happy Number.py
@@ -0,0 +1,33 @@
+'''
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+
+'''
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        if n==1:
+            return True
+        n_set=set()
+        n_set.add(n)
+        while True:
+            cur=0
+            while n>0:
+                cur+=(n%10)**2
+                n=(n-n%10)//10
+            n=cur
+            if n==1:
+                return True
+            if n in n_set:
+                return False
+            n_set.add(n)
