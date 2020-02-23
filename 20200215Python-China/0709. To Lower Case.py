1d81d43872df51f5409489f1ab5a3f1f0c22a80b
zongwu <yangzong5@gmail.com>
2020-02-23 21:21:44 +0800

    Create 0709. To Lower Case.py

---
+++ 20200215Python-China/0709. To Lower Case.py
@@ -0,0 +1,21 @@
+'''
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+ 
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+'''
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
