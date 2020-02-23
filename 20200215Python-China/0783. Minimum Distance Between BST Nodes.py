682e55cd5e0a4bd4b8331acd3c986ef04e304cac
zongwu <yangzong5@gmail.com>
2020-02-23 20:15:54 +0800

    Create 0783. Minimum Distance Between BST Nodes.py

---
+++ 20200215Python-China/0783. Minimum Distance Between BST Nodes.py
@@ -0,0 +1,48 @@
+'''
+Given a Binary Search Tree (BST) with the root node root, return the minimum difference between the values of any two different nodes in the tree.
+
+Example :
+
+Input: root = [4,2,6,1,3,null,null]
+Output: 1
+Explanation:
+Note that root is a TreeNode object, not an array.
+
+The given tree [4,2,6,1,3,null,null] is represented by the following diagram:
+
+          4
+        /   \
+      2      6
+     / \    
+    1   3  
+
+while the minimum difference in this tree is 1, it occurs between node 1 and node 2, also between node 3 and node 2.
+Note:
+
+The size of the BST will be between 2 and 100.
+The BST is always valid, each node's value is an integer, and each node's value is different.
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDiffInBST(self, root: TreeNode) -> int:
+        rep=[]
+        self.getArray(root,rep)
+        gap=rep[1]-rep[0]
+        for k in range(1,len(rep)-1):
+            gap=min(gap,rep[k+1]-rep[k])
+        return gap
+
+
+    def getArray(self,root,rep):
+        if not root:
+            return
+        if root.left:
+            self.getArray(root.left,rep)
+        rep.append(root.val)
+        self.getArray(root.right,rep)
