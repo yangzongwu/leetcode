54bd1326fc2891a8831013dd540131222fdc7502
zongwu <yangzong5@gmail.com>
2020-02-25 14:45:03 +0800

    Create 0637. Average of Levels in Binary Tree.py

---
+++ 20200215Python-China/0637. Average of Levels in Binary Tree.py
@@ -0,0 +1,42 @@
+'''
+Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+Example 1:
+Input:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Output: [3, 14.5, 11]
+Explanation:
+The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+Note:
+The range of node's value is in the range of 32-bit signed integer.
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        if not root:
+            return []
+        root_list=[root]
+        ava_list=[]
+        while root_list:
+            len_root_list=len(root_list)
+            cur_sum=0
+            for _ in range(len_root_list):
+                cur_root=root_list.pop(0)
+                cur_sum+=cur_root.val
+                if cur_root.left:
+                    root_list.append(cur_root.left)
+                if cur_root.right:
+                    root_list.append(cur_root.right)
+            ava_list.append(cur_sum/len_root_list)
+        return ava_list
