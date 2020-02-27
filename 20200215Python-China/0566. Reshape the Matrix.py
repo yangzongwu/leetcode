981a434bc74f5d7480111f766ca5aac58ae9b0f9
zongwu <yangzong5@gmail.com>
2020-02-27 15:18:34 +0800

    Create 0566. Reshape the Matrix.py

---
+++ 20200215Python-China/0566. Reshape the Matrix.py
@@ -0,0 +1,50 @@
+'''
+In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
+
+You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
+
+The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+Example 1:
+Input: 
+nums = 
+[[1,2],
+ [3,4]]
+r = 1, c = 4
+Output: 
+[[1,2,3,4]]
+Explanation:
+The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+Example 2:
+Input: 
+nums = 
+[[1,2],
+ [3,4]]
+r = 2, c = 4
+Output: 
+[[1,2],
+ [3,4]]
+Explanation:
+There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+Note:
+The height and width of the given matrix is in range [1, 100].
+The given r and c are all positive.
+'''
+class Solution:
+    def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if len(nums)*len(nums[0])!=r*c:
+            return nums
+
+        rep=[[0 for _ in range(c)] for _ in range(r)]
+        row,col=0,0
+        for rr in range(r):
+            for cc in range(c):
+                if col==len(nums[0]):
+                    row+=1
+                    col=0
+                rep[rr][cc]=nums[row][col]
+                col+=1
+        
+        return rep
