320bae57ad9417f9e86e2240b7aa77512d1b1470
zongwu <yangzong5@gmail.com>
2020-02-27 17:17:26 +0800

    Create 0766. Toeplitz Matrix.py

---
+++ 20200215Python-China/0766. Toeplitz Matrix.py
@@ -0,0 +1,72 @@
+'''
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+ 
+
+Example 1:
+
+Input:
+matrix = [
+  [1,2,3,4],
+  [5,1,2,3],
+  [9,5,1,2]
+]
+Output: True
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+Input:
+matrix = [
+  [1,2],
+  [2,2]
+]
+Output: False
+Explanation:
+The diagonal "[1, 2]" has different elements.
+
+Note:
+
+matrix will be a 2D array of integers.
+matrix will have a number of rows and columns in range [1, 20].
+matrix[i][j] will be integers in range [0, 99].
+
+Follow up:
+
+What if the matrix is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
+What if the matrix is so large that you can only load up a partial row into the memory at once?
+'''
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        if not matrix:
+            return True
+        
+        if not self.isToeplitzMatrixRowCol(matrix,0,0):
+            return False
+        
+        for col in range(1,len(matrix[0])):
+            if not self.isToeplitzMatrixRowCol(matrix,0,col):
+                return False
+
+        for row in range(1,len(matrix)):
+            if not self.isToeplitzMatrixRowCol(matrix,row,0):
+                return False
+        
+        return True
+
+    def isToeplitzMatrixRowCol(self,matrix,row,col):
+        target=matrix[row][col]
+
+        row+=1
+        col+=1
+        while row<len(matrix) and col<len(matrix[0]):
+            if matrix[row][col]!=target:
+                return False
+            row+=1
+            col+=1
+        return True
+
