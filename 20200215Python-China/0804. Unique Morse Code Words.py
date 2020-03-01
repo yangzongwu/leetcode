c4790117fca09b3444dbd86d52f051f05c114eac
zongwu <yangzong5@gmail.com>
2020-03-01 10:11:20 +0800

    Create 0804. Unique Morse Code Words.py

---
+++ 20200215Python-China/0804. Unique Morse Code Words.py
@@ -0,0 +1,45 @@
+'''
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation: 
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+Note:
+
+The length of words will be at most 100.
+Each words[i] will have length in range [1, 12].
+words[i] will only consist of lowercase letters.
+'''
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        rep=set()
+        morse=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        dict_morse={}
+        strs="abcdefghijklmnopqrstuvwxyz"
+        for i in range(len(strs)):
+            dict_morse[strs[i]]=morse[i]
+        
+        for word in words:
+            cur=""
+            for s in word:
+                cur+=dict_morse[s]
+            rep.add(cur)
+
+        return len(rep)
+
