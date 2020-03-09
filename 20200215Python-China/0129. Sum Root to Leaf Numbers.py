fbd990da55966c3872e1e5bc9331846f5528cec1
zongwu <yangzong5@gmail.com>
2020-03-09 15:29:54 +0800

    Create 0129. Sum Root to Leaf Numbers.py

---
+++ 20200215Python-China/0129. Sum Root to Leaf Numbers.py
@@ -0,0 +1,59 @@
+'''
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input: [1,2,3]
+    1
+   / \
+  2   3
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+Input: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        self.sum=0
+
+        def dfs(root,res):
+            if not root:
+                return
+            if not root.left and not root.right:
+                self.sum+=10*res+root.val
+            if root.left:
+                dfs(root.left,10*res+root.val)
+            if root.right:
+                dfs(root.right,10*res+root.val)
+
+        dfs(root,0)
+        
+        return self.sum
