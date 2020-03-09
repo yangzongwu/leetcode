0496296ce42925cf7c2b27471aac0d3b8975287b
zongwu <yangzong5@gmail.com>
2020-03-09 15:44:52 +0800

    Create 0173. Binary Search Tree Iterator.py

---
+++ 20200215Python-China/0173. Binary Search Tree Iterator.py
@@ -0,0 +1,68 @@
+'''
+Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+
+Calling next() will return the next smallest number in the BST.
+
+ 
+
+Example:
+
+
+
+BSTIterator iterator = new BSTIterator(root);
+iterator.next();    // return 3
+iterator.next();    // return 7
+iterator.hasNext(); // return true
+iterator.next();    // return 9
+iterator.hasNext(); // return true
+iterator.next();    // return 15
+iterator.hasNext(); // return true
+iterator.next();    // return 20
+iterator.hasNext(); // return false
+ 
+
+Note:
+
+next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+You may assume that next() call will always be valid, that is, there will be at least a next smallest number in the BST when next() is called.
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.rep=[]
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            self.rep.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        return self.rep.pop(0)
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return len(self.rep)>0
+
+
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
