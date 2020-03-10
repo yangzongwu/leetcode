cbefe9a0b8cee2687146ffcca60f5bb838bf10df
zongwu <yangzong5@gmail.com>
2020-03-10 19:16:30 +0800

    Create 0450. Delete Node in a BST.py

---
+++ 20200215Python-China/0450. Delete Node in a BST.py
@@ -0,0 +1,75 @@
+'''
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+Note: Time complexity should be O(height of tree).
+
+Example:
+
+root = [5,3,6,2,4,null,7]
+key = 3
+
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Given key to delete is 3. So we find the node with value 3 and delete it.
+
+One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
+
+    5
+   / \
+  4   6
+ /     \
+2       7
+
+Another valid answer is [5,2,6,null,4,null,7].
+
+    5
+   / \
+  2   6
+   \   \
+    4   7
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return root
+        if root.val>key:
+            root.left=self.deleteNode(root.left,key)
+        elif root.val<key:
+            root.right=self.deleteNode(root.right,key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                rootright=root.right
+                rootleft=root.left
+                cur=rootleft
+                while cur.right and cur.right.right:
+                    cur=cur.right
+                if not cur.right:
+                    root=rootleft
+                    rootleft=rootleft.left
+                else:
+                    root=cur.right
+                    cur.right=cur.right.left
+                root.left=rootleft
+                root.right=rootright
+                return root
+
+        return root
