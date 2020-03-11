3cd5409876b60786461345bd7181d9d9182bead0
zongwu <yangzong5@gmail.com>
2020-03-11 15:00:02 +0800

    Create 0654. Maximum Binary Tree.py

---
+++ 20200215Python-China/0654. Maximum Binary Tree.py
@@ -0,0 +1,46 @@
+'''
+Given an integer array with no duplicates. A maximum tree building on this array is defined as follow:
+
+The root is the maximum number in the array.
+The left subtree is the maximum tree constructed from left part subarray divided by the maximum number.
+The right subtree is the maximum tree constructed from right part subarray divided by the maximum number.
+Construct the maximum tree by the given array and output the root node of this tree.
+
+Example 1:
+Input: [3,2,1,6,0,5]
+Output: return the tree root node representing the following tree:
+
+      6
+    /   \
+   3     5
+    \    / 
+     2  0   
+       \
+        1
+Note:
+The size of the given array will be in the range [1,1000].
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
+        max_num=max(nums)
+        
+        loc=0
+        for k in range(len(nums)):
+            if nums[k]==max_num:
+                loc=k
+                break
+        
+        root=TreeNode(nums[loc])
+        root.left=self.constructMaximumBinaryTree(nums[:loc])
+        root.right=self.constructMaximumBinaryTree(nums[loc+1:])
+        return root
