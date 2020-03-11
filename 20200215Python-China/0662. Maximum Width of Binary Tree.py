426d30ab42a523fc07500a06848fadabebe28a0f
zongwu <yangzong5@gmail.com>
2020-03-11 15:15:33 +0800

    Create 0662. Maximum Width of Binary Tree.py

---
+++ 20200215Python-China/0662. Maximum Width of Binary Tree.py
@@ -0,0 +1,84 @@
+'''
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+Example 4:
+
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+
+
+Note: Answer will in the range of 32-bit signed integer.
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        root_list=[[root,1]]
+        rep=0
+        while root_list:
+            rep=max(rep,root_list[-1][1]-root_list[0][1]+1)
+            len_root_list=len(root_list)
+            for _ in range(len_root_list):
+                cur=root_list.pop(0)
+                cur_root=cur[0]
+                cur_val=cur[1]
+                if cur_root.left:
+                    root_list.append([cur_root.left,cur_val*2-1])
+                if cur_root.right:
+                    root_list.append([cur_root.right,cur_val*2])
+        return rep
