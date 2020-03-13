2d4a513b664f43dd7912c8d041a90b64b19a2f17
zongwu <yangzong5@gmail.com>
2020-03-13 17:07:03 +0800

    Create 0071. Simplify Path.py

---
+++ 20200215Python-China/0071. Simplify Path.py
@@ -0,0 +1,53 @@
+'''
+Given an absolute path for a file (Unix-style), simplify it. Or in other words, convert it to the canonical path.
+
+In a UNIX-style file system, a period . refers to the current directory. Furthermore, a double period .. moves the directory up a level. For more information, see: Absolute path vs relative path in Linux/Unix
+
+Note that the returned canonical path must always begin with a slash /, and there must be only a single slash / between two directory names. The last directory name (if it exists) must not end with a trailing /. Also, the canonical path must be the shortest string representing the absolute path.
+
+ 
+
+Example 1:
+
+Input: "/home/"
+Output: "/home"
+Explanation: Note that there is no trailing slash after the last directory name.
+Example 2:
+
+Input: "/../"
+Output: "/"
+Explanation: Going one level up from the root directory is a no-op, as the root level is the highest level you can go.
+Example 3:
+
+Input: "/home//foo/"
+Output: "/home/foo"
+Explanation: In the canonical path, multiple consecutive slashes are replaced by a single one.
+Example 4:
+
+Input: "/a/./b/../../c/"
+Output: "/c"
+Example 5:
+
+Input: "/a/../../b/../c//.//"
+Output: "/c"
+Example 6:
+
+Input: "/a//b////c/d//././/.."
+Output: "/a/b/c"
+'''
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path=path.replace('//','/')
+        path_list=path.split('/')
+        path_stack=[]
+        for p in path_list:
+            if p=='..':
+                if not path_stack:
+                    continue
+                else:
+                    path_stack.pop()
+            elif p and p!='.':
+                path_stack.append(p)
+        
+        rep='/'+'/'.join(path_stack)
+        return rep
