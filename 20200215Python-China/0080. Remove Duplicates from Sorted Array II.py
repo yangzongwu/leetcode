e9598bc5f8ade1ad8de43491af415f9d39771f69
zongwu <yangzong5@gmail.com>
2020-04-09 16:14:19 +0800

    Create 0080. Remove Duplicates from Sorted Array II.py

---
+++ 20200215Python-China/0080. Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,57 @@
+'''
+Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,1,2,3,3],
+
+Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+'''
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums)<=2:
+            return len(nums)
+
+        l,r=0,0
+        while r<len(nums):
+            cur=nums[r]
+            cnt=1
+            r+=1
+            while r<len(nums) and nums[r]==cur:
+                r+=1
+                cnt+=1
+            if cnt==1:
+                nums[l]=cur
+                l+=1
+            else:
+                nums[l]=cur
+                nums[l+1]=cur
+                l+=2
+        return l
