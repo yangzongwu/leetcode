06d299dc5d7656f90be7a2bc0f563c987588b33b
zongwu <yangzong5@gmail.com>
2020-04-13 23:54:01 +0800

    Create 1411. Number of Ways to Paint N × 3 Grid.py

---
+++ 20200215Python-China/1411. Number of Ways to Paint N × 3 Grid.py
@@ -0,0 +1,48 @@
+'''
+You have a grid of size n x 3 and you want to paint each cell of the grid with exactly one of the three colours: Red, Yellow or Green while making sure that no two adjacent cells have the same colour (i.e no two cells that share vertical or horizontal sides have the same colour).
+
+You are given n the number of rows of the grid.
+
+Return the number of ways you can paint this grid. As the answer may grow large, the answer must be computed modulo 10^9 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: 12
+Explanation: There are 12 possible way to paint the grid as shown:
+
+Example 2:
+
+Input: n = 2
+Output: 54
+Example 3:
+
+Input: n = 3
+Output: 246
+Example 4:
+
+Input: n = 7
+Output: 106494
+Example 5:
+
+Input: n = 5000
+Output: 30228214
+ 
+
+Constraints:
+
+n == grid.length
+grid[i].length == 3
+1 <= n <= 5000
+'''
+class Solution:
+    def numOfWays(self, n: int) -> int:
+        if n==1:
+            return 12
+        x,y=6,6
+        #x两个相同，
+        for _ in range(1,n):
+            x,y=3*x+2*y,2*x+2*y
+        return (x+y)%(10**9+7)
