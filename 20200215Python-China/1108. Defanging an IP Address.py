cd3713abda67d1e3100fd51b4a6e0d7137a08d55
zongwu <yangzong5@gmail.com>
2020-04-14 17:31:34 +0800

    Create 1108. Defanging an IP Address.py

---
+++ 20200215Python-China/1108. Defanging an IP Address.py
@@ -0,0 +1,21 @@
+'''
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+ 
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+
+'''
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        address=address.replace('.','[.]')
+        return address
